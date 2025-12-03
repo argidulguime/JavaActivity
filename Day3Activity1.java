99411beb615045fc8e68aeccdb76192d36daeff3
Alain Argi R. Dulguime <aardulguime@headoffice.corp.bpi.com.ph>
2025-12-03 16:57:06 +0800

    initial commit

---
+++ Day3Activity1.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.bpi.hello;
+
+/**
+ * 
+ */
+public class Day3Activity1 {
+
+	/**
+	 * @param args
+	 */
+	public class Book {
+	    String title;
+	    void read() {
+	        System.out.println("Sample only, book: " + title);
+	    }
+	}
+	public class House {
+	    String address;
+	    void location() {
+	        System.out.println("Sample only, house");
+	    }
+	}
+
+	public class Tree {
+	    String typeofTree;
+	    void size() {
+	        System.out.println("Sample only, height ");
+
+	    }
+	
+
+	}
+
+}
